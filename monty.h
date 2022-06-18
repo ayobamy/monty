8dc2ace05dd1384bce1de6b9e5c0aaf40e2c3e24
ayobamy <ayobamyahmed@gmail.com>
2022-06-18 19:31:23 +0000

    [feat]: modified header file

--- monty.h
+++ monty.h
@@ -64,6 +64,7 @@ void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void free_dlistint(stack_t *head);
 void _swap(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
 
 
 #endif
