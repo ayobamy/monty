9f610f66a4e9dc498eabe7c1759d444d3ae0f1e0
ayobamy <ayobamyahmed@gmail.com>
2022-06-18 19:11:01 +0000

    [feat]: modified header file

--- monty.h
+++ monty.h
@@ -60,7 +60,8 @@ void read_file(char *filename, stack_t **stack);
 char *parse_line(char *line, stack_t **stack, unsigned int line_number);
 void _push(stack_t **stack, __attribute__ ((unused))unsigned int line_number);
 void _pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number);
-void _pint(stack_t **stack, unsigned int line_number)
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
 
 
 #endif
